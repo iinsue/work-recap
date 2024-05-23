d90c8b9fc8f4974a80d3a8290a930b1c2078129f
insu <hinsu93@gmail.com>
2024-05-23 15:16:37 +0900

    회원 가입 기능 구현

--- actions/sign-up.ts
+++ actions/sign-up.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 
+import { db } from "@/lib/db";
 import { SignupSchema } from "@/schemas";
 
 export const signup = async (values: z.infer<typeof SignupSchema>) => {
@@ -11,5 +13,28 @@ export const signup = async (values: z.infer<typeof SignupSchema>) => {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent !" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 };
