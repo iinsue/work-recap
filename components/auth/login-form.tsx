d2d9943fb01b93074cf5f46d8e95273ded0a2fbd
insu <hinsu93@gmail.com>
2024-05-23 13:58:54 +0900

    update login form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -11,6 +11,7 @@ import { useForm } from "react-hook-form";
 import { Input } from "../ui/input";
 import { useTransition } from "react";
 import { Button } from "../ui/button";
+import { CardWrapper } from "./card-wrapper";
 
 export const LoginForm = () => {
   const form = useForm();
@@ -18,49 +19,55 @@ export const LoginForm = () => {
 
   return (
     <>
-      <Form {...form}>
-        <form className="space-y-6">
-          <div className="space-y-4">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      type="email"
-                      placeholder="Enter your email"
-                      disabled={isPending}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      type="password"
-                      placeholder="**********"
-                      disabled={isPending}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </div>
-          <Button type="submit" disabled={isPending} className="w-full">
-            Login
-          </Button>
-        </form>
-      </Form>
+      <CardWrapper
+        headerLabel="Welcomback"
+        buttonHref="/sign-up"
+        buttonLabel="계정이 없으신가요?"
+      >
+        <Form {...form}>
+          <form className="space-y-6">
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="email"
+                        placeholder="Enter your email"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="password"
+                        placeholder="**********"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button type="submit" disabled={isPending} className="w-full">
+              Login
+            </Button>
+          </form>
+        </Form>
+      </CardWrapper>
     </>
   );
 };
